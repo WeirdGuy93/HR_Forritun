59a60d75420dc7f380b00302ddd132054caefdaf
WeirdGuy93 <bergura@hotmail.com>
2018-09-04 14:21:03 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,8 @@
+# get length
+# declare counter
+# start with number equal to 1
+# last_number and secondlast_number equal to 0
+# while numbers are less than requested length
+# number = last_number + secondlast_number
+# print number
+# last_number, secondlast_number = number, last_number
