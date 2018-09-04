7fdb2c940dacdf8c17d1bdeeceb7fd6fbadbeb97
WeirdGuy93 <bergura@hotmail.com>
2018-09-04 15:15:29 +0000

    Algorithm written for sequence

--- sequence.py
+++ sequence.py
@@ -1,8 +1,37 @@
 # get length
 # declare counter
-# start with number equal to 1
-# last_number and secondlast_number equal to 0
-# while numbers are less than requested length
-# number = last_number + secondlast_number
+# 
+# 
+# while counter is less than requested length
+# 
+# raise number 
 # print number
+# 
 # last_number, secondlast_number = number, last_number
+#
+# number 1
+# number 2
+# number 3
+# correct number = numbers 1 + 2 + 3
+#
+
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+counter = 0
+number = 1
+number1 = 0
+number2 = 0
+number3 = 0
+
+while counter < n :
+    if number >= 3 :
+        number3 = number2
+    number2 = number1
+    number1 = number
+    
+    number = number3 + number2 + number1
+
+    print(str(number))
+
+    counter += 1
